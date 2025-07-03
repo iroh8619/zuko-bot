d8b65103ce2f3cdc05f591c65fed4d9d429da895
Iroh The Tea Lover <101410810+iroh8619@users.noreply.github.com>
2025-07-03 11:22:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const config = require('./config.json');
 
 
 const sql = new SQLite(path.join(dbDir, 'mainDB.sqlite'));
+initializeDatabase();
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -88,7 +89,6 @@ client.once(Events.ClientReady, () => {
   // Set initial activity and then alternate every 10 seconds
   updateActivity();
   setInterval(updateActivity, 10000);
-  initializeDatabase();
 });
 
 // Slash Command Handling
