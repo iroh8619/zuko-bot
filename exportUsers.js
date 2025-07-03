d1c25b816534a1ef6a66d9fef4142147e367ca62
Iroh The Tea Lover <101410810+iroh8619@users.noreply.github.com>
2025-07-03 10:51:03 +0200

    Update exportUsers.js

--- exportUsers.js
+++ exportUsers.js
@@ -1,7 +1,7 @@
 // export-users.js
 const fs = require('fs');
 const SQLite = require('better-sqlite3');
-const sql = new SQLite('./mainDB.sqlite');
+const sql = new SQLite('/data/mainDB.sqlite');
 
 const guildId = '905876133151637575'; // Replace with your actual guild ID
 const topUsers = sql.prepare("SELECT * FROM levels WHERE guild = ? ORDER BY totalXP DESC;").all(guildId);
