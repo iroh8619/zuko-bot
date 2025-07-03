7de0c2fd4c38de04b61ebb6d6d86cf98767c5691
Iroh The Tea Lover <101410810+iroh8619@users.noreply.github.com>
2025-07-03 11:01:25 +0200

    Update role-level.js

--- commands/role-level.js
+++ commands/role-level.js
@@ -1,4 +1,10 @@
 const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } = require('discord.js');
+const fs = require('fs');
+const dbDir = '/data';
+if (!fs.existsSync(dbDir)) {
+  fs.mkdirSync(dbDir);
+}
+
 const SQLite = require('better-sqlite3');
 const sql = new SQLite('/data/mainDB.sqlite');
 
