eb2613d0f4c3897932441cbf866ef93f1e65cde5
Iroh The Tea Lover <101410810+iroh8619@users.noreply.github.com>
2025-07-03 13:49:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -87,18 +87,7 @@ client.once(Events.ClientReady, () => {
 });
 
 // Slash Command Handling
-client.on(Events.InteractionCreate, async interaction => {
-  if (!interaction.isChatInputCommand()) return;
-  const command = client.commands.get(interaction.commandName);
-  if (!command) return;
 
-  try {
-    await command.execute(interaction, sql);
-  } catch (error) {
-    console.error(error);
-    await interaction.reply({ content: 'There was an error executing this command.', ephemeral: true });
-  }
-});
 
 const fs = require('fs');
 
